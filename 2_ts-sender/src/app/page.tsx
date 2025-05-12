4716a8af76cac261d66266c74dbd82dee7bfbb80
Henrique Menegaz <henrique.menegaz@gmail.com>
2025-05-12 13:46:10 -0700

    finish providers

---
+++ 2_ts-sender/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem' }}> {/* Added some padding for layout */}
+      <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '1rem' }}>
+      </div>
+      <h1>Hi</h1>
+      {/* Rest of your page content */}
+    </main>
+  );
+}
